225e09f6f0bae65a9badea57d769f0a58dd40041
Berk Kasapoglu <berkkasapoglu1@gmail.com>
2023-09-09 20:48:50 +0300

    fix: css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,86 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
-import { config } from './contants';
+import { config } from './constants';
+import { Table } from 'antd';
+
+export enum ReportActionResponse {
+  Buy = 'Buy',
+  Sell = 'Sell',
+}
+
+export interface IReport {
+  ticket: string;
+  price: number;
+  lot: number;
+  date: Date;
+  action: ReportActionResponse;
+}
 
 function App() {
+  const [actionReport, setActionReport] = useState<IReport[]>([]);
+
   useEffect(() => {
     (async () => {
       const res = await axios.get(config.url + '/report');
-      console.log(res);
+
+      setActionReport(res.data);
     })();
   }, []);
 
-  return <></>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%',
+        gap: 50,
+      }}
+    >
+      <Table
+        style={{ width: '100%' }}
+        dataSource={actionReport.map((row, idx) => ({ key: idx, ...row }))}
+        columns={[
+          {
+            title: 'Ticket',
+            dataIndex: 'ticket',
+            key: 'ticket',
+          },
+          {
+            title: 'Price',
+            dataIndex: 'price',
+            key: 'price',
+          },
+          {
+            title: 'Lot',
+            dataIndex: 'lot',
+            key: 'lot',
+          },
+        ]}
+      ></Table>
+      <Table
+        style={{ width: '100%' }}
+        dataSource={actionReport.map((row, idx) => ({ key: idx, ...row }))}
+        columns={[
+          {
+            title: 'Ticket',
+            dataIndex: 'ticket',
+            key: 'ticket',
+          },
+          {
+            title: 'Price',
+            dataIndex: 'price',
+            key: 'price',
+          },
+          {
+            title: 'Lot',
+            dataIndex: 'lot',
+            key: 'lot',
+          },
+        ]}
+      ></Table>
+    </div>
+  );
 }
 
 export default App;
